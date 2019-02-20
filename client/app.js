d936e03022df226f527741afb5694a5c05f3217d
Mubashir Ali Mir <44588455+MMA15@users.noreply.github.com>
2019-02-20 16:37:04 -0500

    Initial commit

---
+++ client/app.js
@@ -0,0 +1,35 @@
+var socket = io(); /*This is saying that socket is now a reference to the Socket.IO library.*/
+$('#history').hide();
+$('.messagearea').hide();
+
+$('.userform').submit(function(e) {
+	e.preventDefault();
+	var username = $('#user').val();
+	socket.emit('login', username, function(usernames) {
+		if (usernames){
+			$('.userform').fadeOut();
+			$('#history').show();
+			$('.messagearea').show();
+			socket.user = username;
+		}else{
+			$('p').html("User already exists. Try again."); //not showing up now, double (olduser msg plus newuser msg on old user screen) thing still happening
+		}
+	});
+	return false;
+});
+
+$('.messagearea').submit(function(e){
+	e.preventDefault();
+	var text = $('#message').val();
+	socket.emit('message', socket.user.bold() + ' says: '+ text);/*The code above says to emit the textual message to the server instead of performing our temporary alert behaviour.*/
+	$('#message').val(''); /* The second line in the code simply clears the input so that another message can be typed by the same user.*/	
+	return false;
+});
+
+socket.on('message', function(msg){
+	$('<li>').text(msg).appendTo('#history');
+});
+
+socket.on('user left', function(number_of_ppl){
+	$('<li>').text("Someone left. There is " + number_of_ppl + " user(s) in the chat.").appendTo('#history');
+});
---
+++ server.js
@@ -0,0 +1,41 @@
+/*console.log('hello from our node script!');shows this on our cmd termnial*/
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+var user_log = [];
+
+app.use('/', express.static('client'));
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+	socket.on('login', function(username, callback){
+		if (user_log.includes(username)){
+			callback(false);			
+		} else{
+			callback(true);
+			user_log.push(username);
+			socket.user = username;
+		}
+	});
+});
+
+io.on('connection', function(socket){
+	socket.on('message', function(msg){
+		io.emit('message', msg);
+	});
+});
+
+io.on('connection', function(socket){
+	socket.on('disconnect', function(){
+		user_log.splice(user_log.indexOf(socket.user), 1);
+		io.emit('user left', user_log.length);
+	});
+});
+
+server.listen(8080, '0.0.0.0', function(){
+	console.log('Chat server running');
+});
+
